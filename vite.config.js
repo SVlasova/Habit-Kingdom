4398d6fb3d326a5799bdcf51e6a0af89e1640d1c
SVlasova <sofyavlasova888@gmail.com>
2026-07-19 12:57:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Habit-Kingdom-English-GitHub-Ready/',
   plugins: [react()],
   server: { host: '127.0.0.1' },
   preview: { host: '127.0.0.1' },
